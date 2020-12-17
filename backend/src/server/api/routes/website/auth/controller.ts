49a2f3fe2b7880e09d6d2b1c961c63df7d0a95b7
WiLLiAM111333 <61476871+WiLLiAM111333@users.noreply.github.com>
2020-12-17 17:10:26 +0100

    ❤

--- backend/src/server/api/routes/website/auth/controller.ts
+++ backend/src/server/api/routes/website/auth/controller.ts
@@ -1,17 +1,6 @@
 import { Controller } from "../../../../../../lib/controller/Controller";
 import { Request, Response } from 'express';
-import { Constants } from "../../../../../utils/constants";
 
 export class AuthController extends Controller {
-  private disallowedRegexes = Constants.AUTH_REGISTER_REGEXES;
 
-  public constructor() {
-    super();
-  }
-
-  public registerUser(): (req: Request, res: Response) => void {
-    return (req, res) => {
-      
-    }
-  }
 }
